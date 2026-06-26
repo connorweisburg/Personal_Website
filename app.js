d8085a6a413f51740c6dc9808bd6fb0fc6bd21da
Connor <connorweisburg@gmail.com>
2026-06-25 22:23:45 -0700

    test server error

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-console.log("MARKED VERSION:", require("marked/package.json").version);
+// const express = require("express");
+// const path = require("path");
+
+// const { getAllPosts, getPostBySlug } = require("./lib/posts");
+
+// const app = express();
+
+// // Static files
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // EJS setup
+// app.set("view engine", "ejs");
+
+// // Home
+// app.get("/", (req, res) => {
+//     res.render("index");
+// });
+
+// // Music
+// app.get("/music", (req, res) => {
+//     res.render("music");
+// });
+
+// // Professional (BLOG LIST)
+// app.get("/professional", (req, res) => {
+//     const posts = getAllPosts();
+//     res.render("professional", { posts });
+// });
+
+// // Professional (SINGLE POST)
+// app.get("/professional/:slug", (req, res) => {
+//     const post = getPostBySlug(req.params.slug);
+
+//     if (!post) {
+//         return res.status(404).send("Post not found");
+//     }
+
+//     res.render("post", { post });
+// });
+
+// // Other
+// app.get("/other", (req, res) => {
+//     res.render("other");
+// });
+
+// // // Start server
+// // const PORT = 3000;
+
+// // app.listen(PORT, () => {
+// //     console.log(`Server running at http://localhost:${PORT}`);
+// // });
+
+// module.exports = app;
+
+
+
+
 
 const express = require("express");
-const path = require("path");
-
-const { getAllPosts, getPostBySlug } = require("./lib/posts");
 
 const app = express();
 
-// Static files
-app.use(express.static(path.join(__dirname, "public")));
-
-// EJS setup
-app.set("view engine", "ejs");
-
-// Home
 app.get("/", (req, res) => {
-    res.render("index");
+  res.send("SERVER IS ALIVE");
 });
-
-// Music
-app.get("/music", (req, res) => {
-    res.render("music");
-});
-
-// Professional (BLOG LIST)
-app.get("/professional", (req, res) => {
-    const posts = getAllPosts();
-    res.render("professional", { posts });
-});
-
-// Professional (SINGLE POST)
-app.get("/professional/:slug", (req, res) => {
-    const post = getPostBySlug(req.params.slug);
-
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-
-    res.render("post", { post });
-});
-
-// Other
-app.get("/other", (req, res) => {
-    res.render("other");
-});
-
-// // Start server
-// const PORT = 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
 
 module.exports = app;
